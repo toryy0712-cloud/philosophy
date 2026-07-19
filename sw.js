0e4d4f44ccd2f9185e8e2ef66d6e5b4340b33d87
toryy0712-cloud <toryy0712@gmail.com>
2026-07-19 18:02:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE_NAME = 'philosophy-chat-v1';
+const CACHE_NAME = 'philosophy-chat-v2';
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/',
-                '/index.html',
-                '/chat.html',
+                '/chat',  /* .html 확장자를 제거하여 Vercel의 cleanUrls와 충돌을 막습니다 */
                 '/manifest.json'
             ]);
         })
